799c1e652ad6e9773b634b8eaee68a3d3f7a864b
Kreg101 <egorkravchenko321@yandex.ru>
2023-07-08 21:52:54 +0300

    complete iter4 #2

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -9,7 +9,7 @@ var (
 )
 
 func parseFlags() {
-	flag.StringVar(&endpoint, "a", "http://localhost:8080", "address and port to run server")
+	flag.StringVar(&endpoint, "a", "localhost:8080", "address and port to run server")
 	flag.IntVar(&reportInterval, "r", 10, "frequency of sending metrics on server in seconds")
 	flag.IntVar(&pollInterval, "p", 2, "frequency of updating metrics in seconds")
 	flag.Parse()
