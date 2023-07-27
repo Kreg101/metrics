e6f90d53d0d7b661006cd648f7523370da1c0f66
Kreg101 <egorkravchenko321@yandex.ru>
2023-07-27 13:32:35 +0300

    replace resty client with simple http.Post()

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"github.com/Kreg101/metrics/internal/agent"
-	"time"
 )
 
 func main() {
 
 	parseFlags()
-	for i := 0; i < 10; i++ {
-		go func() {
-			a := agent.NewAgent(flagPollInterval, flagReportInterval, "http://"+flagEndpoint)
-			a.Start()
-		}()
-	}
 
-	time.Sleep(1000 * time.Second)
+	a := agent.NewAgent(flagPollInterval, flagReportInterval, "http://"+flagEndpoint)
+	a.Start()
 
 }
