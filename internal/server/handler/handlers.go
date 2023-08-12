78c1b1999b96fc8a3745debf34b078dd0791c639
Kreg101 <egorkravchenko321@yandex.ru>
2023-08-12 10:10:01 +0300

    change updates handler

--- internal/server/handler/handlers.go
+++ internal/server/handler/handlers.go
@@ -148,7 +148,7 @@ func (mux *Mux) ping(w http.ResponseWriter, r *http.Request) {
 }
 
 func (mux *Mux) updates(w http.ResponseWriter, r *http.Request) {
-	var metrics []metric.Metric
+	var metrics *[]metric.Metric
 
 	err := json.NewDecoder(r.Body).Decode(&metrics)
 	if err != nil {
@@ -157,7 +157,7 @@ func (mux *Mux) updates(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, m := range metrics {
+	for _, m := range *metrics {
 		if (m.MType == "counter" && m.Delta == nil) || (m.MType == "gauge" && m.Value == nil) {
 			mux.log.Errorf("empty delta or value in request")
 		}
